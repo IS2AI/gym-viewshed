eedac261771c6a53ffac5e59c3d83e379a168b81
baimukashev <dbaimukashev@gmail.com>
2020-02-03 14:57:29 +0600

    initial setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='gym-viewshed',
+      version='1.0',
+      description='gym-viewshed custom gym environment for ArcGIS',
+      author='Daulet Baimukashev',
+      author_email='dbaimukashev@gmail.com',
+      #packages=[""]
+      )
