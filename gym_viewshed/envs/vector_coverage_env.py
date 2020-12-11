083c0dd914e897277e95133d3c3a9d600062867a
baimukashev <dbaimukashev@gmail.com>
2020-12-11 13:49:19 +0600

    local changes

--- gym_viewshed/envs/vector_coverage_env.py
+++ gym_viewshed/envs/vector_coverage_env.py
@@ -115,6 +115,7 @@ class VectorCoverageEnv(gym.Env):
         self.reward_good_step = 1
         self.reward_bad_step = -0.05
         self.max_iter = 280
+        self.reward_temp = 0
 
         # coverage
         self.city_coverage = np.asarray(Image.open(r"../data/images/RasterTotalCoverage4.png"))
@@ -144,10 +145,12 @@ class VectorCoverageEnv(gym.Env):
         crossed_points = (crossed_map > 0).astype(int)
         crossed_area = crossed_points.sum()
 
-        if crossed_area > 0:
-            reward = 1
-        else:
-            reward = 0
+        reward = crossed_area
+        self.reward_temp = reward
+        # if crossed_area > 0:
+        #     reward = 1
+        # else:
+        #     reward = 0
 
         #done ?
         if self.iteration > self.max_iter:
@@ -186,8 +189,20 @@ class VectorCoverageEnv(gym.Env):
     def reset(self):
         print('Env reset ...')
         self.iteration = 0
+
+        # position
+        self.pan_pos = 0
+        self.tilt_pos = -45
+        self.zoom_pos = 20
+
+        self.horizon_fov = self.horizon_fov_max
+        self.vertical_fov =  self.vertical_fov_max
+        self.zoom_distance = self.max_distance_min_zoom
+
+        # state
         self.state_visible_points = np.zeros((self.im_height, self.im_width))
-        next_state = np.stack((self.city_coverage, self.state_visible_points), axis = 0)
+        self.state_total_coverage = self.city_coverage
+        next_state = np.stack((self.state_total_coverage/255.0, self.state_visible_points), axis = 0)
 
         return next_state
 
@@ -225,7 +240,7 @@ class VectorCoverageEnv(gym.Env):
                 "5": "zoom out (0.8x)"
                 }
 
-            action_display = 'Last Action: {}'.format(action_dict[str(self.action)])
+            action_display = 'Last Action: {},         Reward: {}'.format(action_dict[str(self.action)], self.reward_temp)
 
             text_display = 'Pan: {}, Tilt: {}, Zoom: {:.2f}'.format(
                             self.pan_pos, self.tilt_pos, self.zoom_pos/20)
@@ -263,7 +278,7 @@ class VectorCoverageEnv(gym.Env):
             cv2.imshow("coverage", show_array2)
 
 
-            cv2.waitKey(10)
+            cv2.waitKey(5)
 
         except KeyboardInterrupt:
             cv2.destroyAllWindows()
